734692d269299e5a4e9e04aafb48fa02eea01b1d
yinzhiqianga <yinzhiqianga@enn.cn>
2022-03-22 09:33:19 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,30 +13,14 @@ module.exports = {
   themeConfig: {
     sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
     nav:[ // 导航栏配置
-      {text: 'MyBatis', link: '/mybatis/' },
       {text: '搭建博客', link: '/vuepress/' },
       {text: 'github', link: 'https://github.com/jasonyen92' }
     ],
     sidebar: {  //侧边拦
-             '/mybatis/': [
-                {
-                    title: 'mybatis',
-                    children: [
-                        {title: 'mybatis基础', path: '/mybatis/mybatis基础/使用VuePress搭建个人博客'}
-                        
-                    ]
-                },
-                {
-                    title: '手撸mybatis',
-                    children: [
-                        {title: '手撸mybatis', path: '/mybatis/手撸mybatis/使用VuePress搭建个人博客'},
-                    ]
-                }
-            ],
             '/vuepress/': [{
                 title: '手把手教你搭建博客',
                 children: [
-                    {title: '手把手教你搭建博客', path: '/vuepress/搭建博客一/使用VuePress搭建个人博客'},
+                    {title: '手把手教你搭建博客', path: '/vuepress/搭建博客一/使用VuePress搭建个人博客'}
                 ]
             }]
       }
